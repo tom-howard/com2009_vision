25327ecb2eb01ea345bae73964e80d100b2015b5
Tom Howard <t.howard@sheffield.ac.uk>
2021-03-15 14:57:06 +0000

    Tidy up and added a .gitignore

--- install/week6.sh
+++ install/week6.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
+echo "[Info] Installing additional Python packages..."
 pip install matplotlib
 
+echo "[Info] Creating a week6_images directory in ~/myrosdata/ ..."
 mkdir -p ~/myrosdata/week6_images/
 
-cd ~/catkin_ws/src
-
+echo "[Info] Cloning the GitHub repo for Week 6 (com2009_vision)..."
+cd ~/catkin_ws/src/
 git clone https://github.com/tom-howard/com2009_vision.git
-
 cd ./com2009_vision/src/
-
 chmod +x image_colours.py colour_search.py
 
-cp ./models/line_following_track ~/.gazebo/models/
+echo "[Info] Copying Gazebo model files..."
+cp -r ./models/line_following_track ~/.gazebo/models/
+
+echo "[Info] Running catkin_make (for good measure)..."
+cd ~/catkin_ws/
+
+catkin_make
+
+echo "[Info] Done."
