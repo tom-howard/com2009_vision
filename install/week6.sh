391d06958710ebcdc58573158eee2af8b8c050b9
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-03-15 15:37:13 +0000

    Update week6.sh

--- install/week6.sh
+++ install/week6.sh
@@ -14,7 +14,7 @@ chmod +x image_colours.py colour_search.py
 
 echo "[Info] Copying Gazebo model files..."
 mkdir -p ~/.gazebo/models/
-cp -r ./models/line_following_track ~/.gazebo/models/
+cp -r ../models/line_following_track ~/.gazebo/models/
 
 echo "[Info] Running catkin_make (for good measure)..."
 cd ~/catkin_ws/
